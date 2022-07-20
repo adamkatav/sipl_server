ddef66425c0298b93c5e5e78462939dab6b76523
Adam Katav <adamkatav@campus.technion.ac.il>
2022-07-20 16:47:40 +0300

    Ready for Dan's approval

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+#from msilib.schema import File
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 import sys
+import subprocess
 
 FTP_PORT = 2121
 
@@ -15,13 +17,50 @@ FTP_PASSWORD = sys.argv[2]
 FTP_DIRECTORY = "./ftp_files/"
 
 
+class MyHandler(FTPHandler):
+
+    # def on_connect(self):
+    #     print(f"{self.remote_ip}:{self.remote_port} connected")
+
+    # def on_disconnect(self):
+    #     # do something when client disconnects
+    #     pass
+
+    # def on_login(self, username):
+    #     # do something when user login
+    #     pass
+
+    # def on_logout(self, username):
+    #     # do something when user logs out
+    #     pass
+
+    # def on_file_sent(self, file):
+    #     # do something when a file has been sent
+    #     pass
+
+    def on_file_received(self, file: str):
+        subprocess.run('python', './sipl_spice/script.py', file)
+        pass
+
+    # def on_incomplete_file_sent(self, file):
+    #     # do something when a file is partially sent
+    #     pass
+
+    # def on_incomplete_file_received(self, file):
+    #     # remove partially uploaded files
+    #     import os
+    #     os.remove(file)
+
+
+
+
 def main():
     authorizer = DummyAuthorizer()
 
     # Define a new user having full r/w permissions.
     authorizer.add_user(FTP_USER, FTP_PASSWORD, FTP_DIRECTORY, perm='elradfmw')
 
-    handler = FTPHandler
+    handler = MyHandler
     handler.authorizer = authorizer
 
     # Define a customized banner (string returned when client connects)
