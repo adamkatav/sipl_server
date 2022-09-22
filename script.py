a13c6e0551a9f4b7831167ed718a533ade7644ec
Adam Katav <adamkatav@campus.technion.ac.il>
2022-09-22 15:12:58 +0300

    using Dans script in docker

--- script.py
+++ script.py
@@ -2,33 +2,36 @@
 
 import os
 import subprocess
+import sys
 import time
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-loading_proc = subprocess.Popen(['python', 'loading.py'])
-time.sleep(4)
+# loading_proc = subprocess.Popen(['python', 'loading.py'])
+# time.sleep(4)
 
-# set the timer interval 5000 milliseconds
-fig = plt.figure(figsize=(10, 10), dpi=80)
-timer = fig.canvas.new_timer(interval = 3500)
-timer.add_callback(plt.close)
+# # set the timer interval 5000 milliseconds
+# fig = plt.figure(figsize=(10, 10), dpi=80)
+# timer = fig.canvas.new_timer(interval = 3500)
+# timer.add_callback(plt.close)
 
-img = mpimg.imread('/home/adam/Desktop/sipl/sipl_server/sipl_spice/test_1/black_label_image.jpeg')
-plt.imshow(img)
-plt.axis('off')
-timer.start()
-plt.show()
-
-
-img = mpimg.imread('/home/adam/Desktop/sipl/sipl_server/sipl_spice/test_1/101_17_1_bw.jpeg')
-plt.imshow(img)
-plt.axis('off')
-timer.start()
-plt.show()
+# img = mpimg.imread('/home/adam/Desktop/sipl/sipl_server/sipl_spice/test_1/black_label_image.jpeg')
+# plt.imshow(img)
+# plt.axis('off')
+# timer.start()
+# plt.show()
 
 
-ltspice_proc = subprocess.Popen(['wine', '/home/adam/.wine/drive_c/Program Files/LTC/LTspiceXVII/XVIIx64.exe', '-big', '-b', 'C:\\users\\adam\\Desktop\\sipl\\sipl_server\\sipl_spice\\test_1\\101_17_1.asc'])
+# img = mpimg.imread('/home/adam/Desktop/sipl/sipl_server/sipl_spice/test_1/101_17_1_bw.jpeg')
+# plt.imshow(img)
+# plt.axis('off')
+# timer.start()
+# plt.show()
+subprocess.run(['cp', sys.argv[1], '../pythonProject/Dans_rebuild/pic.jpg'])
+subprocess.run(['docker', 'exec', 'spice', './run_dans_script.sh', 'pic.jpg'])
+subprocess.run(['rm', sys.argv[1]])
+
+ltspice_proc = subprocess.Popen(['wine', '/home/adam/.wine/drive_c/Program Files/LTC/LTspiceXVII/XVIIx64.exe', '-big', '-b', f'C:\\users\\adam\\Desktop\\sipl\\pythonProject\\Dans_rebuild\\output.asc'])
 # os.system("wine '/home/adam/.wine/drive_c/Program Files/LTC/LTspiceXVII/XVIIx64.exe' -big -b 'C:\\users\\adam\\Desktop\\sipl\\sipl_server\\sipl_spice\\adam_circuit.asc'")
-loading_proc.kill()
+# loading_proc.kill()
